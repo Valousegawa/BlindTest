9b1bcb6917bc90a56e6bd6716f85392f816133be
Valentin <valou.pannacotta@gmail.com>
2017-04-27 11:26:04 +0200

    Changed Form name and ID

--- Main/Form1.Designer.cs
+++ Main/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Main
 {
-    partial class Form1
+    partial class form_main
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(398, 366);
+            this.Name = "form_main";
+            this.Text = "BlindTest";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
