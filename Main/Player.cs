97798492f08e3ff3a7acc2b492765f8311288aaf
Valentin <valou.pannacotta@gmail.com>
2017-05-03 10:13:43 +0200

    Gestion de l'interface de gameplay

---
+++ Main/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Player
+    {
+        String pseudo;
+        int is_arbitre;
+        int id_team;
+        int score;
+
+        public Player()
+        {
+            this.Is_arbitre = 0;
+            this.Id_team = 0;
+            this.Score = 0;
+        }
+
+        public Player(String pseudo, int is_arbitre, int id_team, int score)
+        {
+            this.Pseudo = pseudo;
+            this.Is_arbitre = is_arbitre;
+            this.Id_team = id_team;
+            this.Score = score;
+        }
+
+        public string Pseudo { get => pseudo; set => pseudo = value; }
+        public int Is_arbitre { get => is_arbitre; set => is_arbitre = value; }
+        public int Id_team { get => id_team; set => id_team = value; }
+        public int Score { get => score; set => score = value; }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class form_players : Form
+    {
+
+        //Constants
+        const int AW_SLIDE = 0X40000;
+        const int AW_HOR_POSITIVE = 0X1;
+        const int AW_HOR_NEGATIVE = 0X2;
+        const int AW_BLEND = 0X80000;
+
+        [DllImport("user32")]
+        static extern bool AnimateWindow(IntPtr hwnd, int time, int flags);
+
+        public form_players()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            //Load the Form At Position of Main Form
+            int WidthOfMain = Application.OpenForms["form_main"].Width;
+            int HeightofMain = Application.OpenForms["form_main"].Height;
+            int LocationMainX = Application.OpenForms["form_main"].Location.X;
+            int locationMainy = Application.OpenForms["form_main"].Location.Y;
+
+            //Set the Location
+            this.Location = new Point(LocationMainX + WidthOfMain, locationMainy + 10);
+
+            //Animate form
+            AnimateWindow(this.Handle, 500, AW_SLIDE | AW_HOR_POSITIVE);
+        }
+    }
+}
